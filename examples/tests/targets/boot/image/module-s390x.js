35462d68667f7211ed2b348448f9c66960db8fff
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2025-03-25 11:52:22 +0900

    Add tests for wasm
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>

---
+++ examples/tests/targets/boot/image/module-s390x.js
@@ -0,0 +1,10 @@
+Module['arguments'] = [
+    '-nographic', '-m', '512M', '-accel', 'tcg,tb-size=500',
+    //Use the following to enable MTTCG
+    //'-nographic', '-m', '512M', '-accel', 'tcg,tb-size=500,thread=multi', '-smp', '4,sockets=4',
+    '-L', '/pack/',
+    '-nic', 'none',
+    '-drive', 'if=virtio,format=raw,file=/pack/rootfs.bin',
+    '-kernel', '/pack/Image',
+    '-append', 'earlyprintk=ttyS0 console=ttyS0 root=/dev/vda rootwait ro loglevel=7',
+];
